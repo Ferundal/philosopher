4e9629b0e373c5cf2b9ff424bb28ac1182229c4a
ferundal <ferundal@gmail.com>
2021-09-12 19:13:44 +0300

    fixed shit 2

--- philo/src/philo_start.c
+++ philo/src/philo_start.c
@@ -35,8 +35,8 @@ void	wait_start(t_p_arg *p_a)
 	long long int	dealay;
 
 	start = &p_a->c_info->start;
-	if (p_a->c_info->time_to_e * 2 + p_a->c_info->time_to_s - \
-		p_a->c_info->time_to_d > 0 && p_a->c_info->time_to_e * 2 \
+	if (p_a->c_info->time_to_e * 2 + p_a->c_info->time_to_s \
+		> p_a->c_info->time_to_d + 10 && p_a->c_info->time_to_e * 2 \
 		< p_a->c_info->time_to_d && p_a->c_info->philo_amnt % 2 == 1)
 		special_start(&dealay, p_a);
 	else
